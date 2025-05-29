e7bb98478c1214f264ce1494f252bb1bbb684007
a3malik <amit26klm@gmail.com>
2025-05-30 01:25:23 +0530

    using pydantic

--- mybookapp.py
+++ mybookapp.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
 class Book:
-    def __init__(self, id, title, author, decsription, rating):
+
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: float
+
+    def __init__(self, id, title, author, description, rating):
         self.id = id
         self.title = title
         self.author = author
-        self.decsription = decsription
+        self.description = description
         self.rating = rating
 
     def get_book(self):
@@ -15,6 +23,13 @@ class Book:
             "title": self.title,
             "author": self.author,
         }
+    
+class BookRequest(BaseModel):
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: float
     
 BOOKS = [
     Book(1, "Godan", "Munshi Premchand", "A novel by Munshi Premchand", 4.5),
@@ -27,9 +42,11 @@ BOOKS = [
 
 @app.get("/books")
 async def get_all_books():
-    return [ book.get_book() for book in BOOKS ]
+    #return [ book.title for book in BOOKS ]
+    return BOOKS
 
 @app.post("/create_book")
-async def create_book(x=Body()):
-    new_book = Book(x.get('id'), x.get('title'), x.get('author'), x.get('decsription'), x.get('rating'))
+async def create_book(book_request: BookRequest):
+    #BOOKS.append(book_request)
+    new_book=Book(**book_request.model_dump())
     BOOKS.append(new_book)
