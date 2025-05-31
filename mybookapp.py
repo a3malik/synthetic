9966236afcc2d0edb78182ff65f995e038d1e3c3
a3malik <amit26klm@gmail.com>
2025-06-01 01:24:38 +0530

    added understanding of **dict to extra.py file

--- mybookapp.py
+++ mybookapp.py
@@ -30,6 +30,15 @@ class BookRequest(BaseModel):
     author: str
     description: str
     rating: float
+
+class SmallRequest(BaseModel):
+    id: int
+    title: str
+
+class SmallBook():
+    def __init__(self, id: int, title: str):
+        self.id = id
+        self.title = title
     
 BOOKS = [
     Book(1, "Godan", "Munshi Premchand", "A novel by Munshi Premchand", 4.5),
@@ -38,15 +47,32 @@ BOOKS = [
     Book(4, "Rajtarangini", "kalhan", "History of Indian Royal families", 4.0),
     Book(5, "Mansarovar", "Munshi Premchand", "Collection of stories", 4.0),
 ]
-#{"id":6, "title":"Raseedi Tikat", "author":"Amrita Preetam", "description":"Autobiograpdy", "rating":4.0}
+
+SMALLBOOKS = [
+    SmallBook(1, "Godan"),
+    SmallBook(2, "kamayani"),
+    SmallBook(3, "Hindi Sahitya Ka Itihas"),
+    SmallBook(4, "Rajtarangini"),
+    SmallBook(5, "Mansarovar"),
+]
+
+'''
+{"id":6, 
+ "title":"Raseedi Tikat", 
+ "author":"Amrita Preetam", 
+ "description":"Autobiograpdy", 
+ "rating":4.0
+}
+'''
+
 
 @app.get("/books")
 async def get_all_books():
-    #return [ book.title for book in BOOKS ]
-    return BOOKS
+    return [ book.title for book in SMALLBOOKS ]
+    #return BOOKS
 
 @app.post("/create_book")
-async def create_book(book_request: BookRequest):
+async def create_book(book_request: SmallRequest):
     #BOOKS.append(book_request)
-    new_book=Book(**book_request.model_dump())
-    BOOKS.append(new_book)
+    new_book=SmallBook(**book_request.model_dump())
+    SMALLBOOKS.append(new_book)
