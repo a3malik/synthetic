7f8db6c0ce90a158cf1add9a0b76bc9dcc603654
a3malik <amit26klm@gmail.com>
2025-06-01 14:34:04 +0530

    added pydantic model config

--- validbookapp.py
+++ validbookapp.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel,Field
+from typing import Optional
 
 app = FastAPI()
 
@@ -32,20 +33,26 @@ class BookRequest(BaseModel):
     rating: float = Field(gt=0, le=5)
 
 class SmallRequest(BaseModel):
-    id: int
+    id: Optional[int] = Field(default=None)
     title: str = Field(min_length=1)
     author: str = Field(min_length=1)
-    #rating: float = Field(gt=0, le=5)
+    rating: int = Field(gt=0, le=5)
+
+    model_config = {
+        "json_schema_extra":{
+            "example":{"title":"A New Book","author":"Amit","rating":1}
+        }
+    }
 
 class SmallBook():
-    def __init__(self, id: int, title: str, author: str):
+    def __init__(self, id: int, title: str, author: str, rating: int):
         self.id = id
         self.title = title
         self.author = author
+        self.rating = rating
 
     def get_book(self):
         return {
-            "id": self.id,
             "title": self.title,
             "author": self.author
         }
@@ -59,9 +66,9 @@ BOOKS = [
 ]
 
 SMALLBOOKS = [
-    SmallBook(1,"Godan", "Premchand"),
-    SmallBook(2,"Rajtarangini", "kalhan"),
-    SmallBook(3,"Mansarovar", "Premchand"),
+    SmallBook(1,"Godan", "Premchand",4),
+    SmallBook(2,"Rajtarangini", "kalhan",5),
+    SmallBook(3,"Mansarovar", "Premchand",4),
 ]
 
 '''
@@ -79,16 +86,43 @@ async def get_all_books():
     return [ book.get_book() for book in SMALLBOOKS ]
     #return [ book.get_book() for book in BOOKS ]
 
+@app.get("/books/{bookid}")
+async def get_book_by_id(bookid: int):
+    '''We need to use int function if we do not specify bookid to be int
+      in the parameter above'''
+    #return [ book.get_book() for book in SMALLBOOKS if book.id == int(bookid) ]
+    return [ book.get_book() for book in SMALLBOOKS if book.id == bookid ]
+
+@app.get("/books/")
+async def get_book_by_rating(rating: int):
+    return [x.get_book() for x in SMALLBOOKS if x.rating == rating]
+
 @app.post("/create_book")
 async def create_book(book_request: SmallRequest):
     new_book=SmallBook(**book_request.model_dump())
     #new_book=Book(**book_request.model_dump())
-    SMALLBOOKS.append(find_book_id(new_book))
+    SMALLBOOKS.append(find_book_by_id(new_book))
 
-def find_book_id(book: SmallBook):
-    if len(SMALLBOOKS)>0:
-        book.id = SMALLBOOKS[-1].id + 1
-        #book.id = BOOKS[-1].id + 1
-    else:
-        book.id = 1
+def find_book_by_id(book: SmallBook):
+    book.id = 1 if len(SMALLBOOKS) == 0 else SMALLBOOKS[-1].id + 1
     return book
+
+@app.put("/books/update_book")
+async def update_book(book_request: SmallRequest):
+    for book in SMALLBOOKS:
+        if book.id == book_request.id:
+            book.title = book_request.title
+            book.author = book_request.author
+            book.rating = book_request.rating
+            return {"message": "Book updated successfully"}
+
+    '''for i in range(len(SMALLBOOKS)):
+        if SMALLBOOKS[i].id == book_request.id:
+            SMALLBOOKS[i] = book_request'''
+
+@app.delete("/books/delete_book/")
+async def delete_book(bookid: int):
+    for i in range(len(SMALLBOOKS)):
+        if SMALLBOOKS[i].id == bookid:
+            del SMALLBOOKS[i]
+            return {"message": "Book deleted successfully"}
