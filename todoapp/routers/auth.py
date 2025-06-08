1454d7478c37c4194828830866877372070cfd25
a3malik <amit26klm@gmail.com>
2025-06-08 08:22:23 +0530

    user authentication using JWT

--- todoapp/routers/auth.py
+++ todoapp/routers/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from models import Users
 from passlib.context import CryptContext
@@ -6,10 +6,20 @@ from database import SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
 from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt
+from datetime import datetime, timedelta, timezone
 
-router = APIRouter()
 
-bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+router = APIRouter(
+    prefix='/auth',
+    tags=['auth'],
+)
+
+
+SECRET_KEY = '9abb2343xjj329v798xjj99898e0909xjj9900r1333jhh242xjjy32323'
+ALGORITHM = 'HS256'
+
 
 def get_db():
     db = SessionLocal()
@@ -18,7 +28,42 @@ def get_db():
     finally:
         db.close()
 
+
+#Define dependencies to be used in the route handlers
 db_dependency = Annotated[Session, Depends(get_db)]
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
+
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    encode = {'sub':username, 'id': user_id}
+    expires = datetime.now(timezone.utc) + expires_delta
+    encode.update({'exp':expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail='Could not validate user.',)
+        return {'username': username, 'id': user_id}
+    except jwt.JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Could not validate user.')
+
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -29,7 +74,12 @@ class CreateUserRequest(BaseModel):
     role: str
 
 
-@router.post("/auth",status_code=status.HTTP_201_CREATED)
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+@router.post("/",status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = Users(
         username=create_user_request.username,
@@ -42,3 +92,14 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
     )
     db.add(create_user_model)
     db.commit()
+
+
+@router.post("/token", response_model=Token)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail='Could not validate user.',)
+    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    return {'access_token':token, 'token_type':'bearer'}
--- todoapp/routers/todos.py
+++ todoapp/routers/todos.py
@@ -8,6 +8,7 @@ from typing import Annotated
 from starlette import status
 from pydantic import BaseModel, Field
 from routers import auth
+from .auth import get_current_user
 
 router = APIRouter()
 
@@ -18,38 +19,60 @@ def get_db():
     finally:
         db.close()
 
+
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
 
 class TodoRequest(BaseModel):
     title: str = Field(min_length=3)
     description: str = Field(min_length=3,max_length=50)
     priority: int = Field(gt=0, lt=6)
     complete: bool
-    owner_id: int = Field(gt=0)
+
 
 @router.get("/",status_code=status.HTTP_200_OK)
-async def read_all(db: Annotated[Session, Depends(get_db)]):
-    return db.query(Todos).all()
+async def read_all(user: user_dependency, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    return db.query(Todos).filter(Todos.owner_id == user.get('id')).all()
+
 
 @router.get("/todos/{todo_id}",status_code=status.HTTP_200_OK)
-async def read_todo_by_id(db: db_dependency, todo_id: int=Path(gt=0)):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+async def read_todo_by_id(user: user_dependency, db: db_dependency, todo_id: int=Path(gt=0)):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+
+    todo_model = db.query(Todos).filter(Todos.id == todo_id)\
+        .filter(Todos.owner_id == user.get('id')).first()
     if todo_model is not None:
         return todo_model
     return HTTPException(status_code=404, detail='Todo not found')
 
+
 @router.post("/todo", status_code=status.HTTP_201_CREATED)
-async def create_todo(db: db_dependency, todo_request: TodoRequest):
-    todo_model = Todos(**todo_request.model_dump())
+async def create_todo(user: user_dependency,
+                      db: db_dependency,
+                      todo_request: TodoRequest
+                      ):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    todo_model = Todos(**todo_request.model_dump(),owner_id=user.get('id'))
+
     db.add(todo_model)
     db.commit()
 
+
 @router.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def update_todo_by_id( db: db_dependency,
-                       todo_request: TodoRequest,
-                       todo_id: int = Path(gt=0)
+async def update_todo_by_id( user: user_dependency,
+                            db: db_dependency,
+                            todo_request: TodoRequest,
+                            todo_id: int = Path(gt=0)
                        ):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    todo_model = db.query(Todos).filter(Todos.id == todo_id)\
+        .filter(Todos.owner_id == user.get('id')).first()
     if todo_model is None:
         raise HTTPException(status_code=404, detail='Todo not found')
 
@@ -57,17 +80,23 @@ async def update_todo_by_id( db: db_dependency,
     todo_model.description = todo_request.description
     todo_model.priority = todo_request.priority
     todo_model.complete = todo_request.complete
-    todo_model.owner_id = todo_request.owner_id
 
     db.add(todo_model)
     db.commit()
 
 
 @router.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_todo_by_id(db: db_dependency, todo_id:int = Path(gt=0)):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+async def delete_todo_by_id(user: user_dependency,
+                            db: db_dependency,
+                            todo_id:int = Path(gt=0)
+                            ):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    todo_model = db.query(Todos).filter(Todos.id == todo_id)\
+        .filter(Todos.owner_id == user.get('id')).first()
     if todo_model is None:
         raise HTTPException(status_code=404, detail='Todo not found')
+    db.query(Todos).filter(Todos.id == todo_id)\
+        .filter(Todos.owner_id == user.get('id')).delete()
 
-    db.query(Todos).filter(Todos.id == todo_id).delete()
     db.commit()
