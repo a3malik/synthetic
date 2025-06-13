d3cd5475c9ada59f60a2ae003a42fccbce4566cc
a3malik <amit26klm@gmail.com>
2025-06-13 22:08:07 +0530

    added support for production databases

--- todoapp/database.py
+++ todoapp/database.py
@@ -5,8 +5,12 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+#SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:urpasswd@localhost/TodoApplicationDatabase'
+#SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:urpasswd@localhost:3306/TodoApplicationDatabase'
 
+#This connect_args is for sqlite only.
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
+#engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
---
+++ todoapp/routers/users.py
@@ -0,0 +1,4 @@
+# create two API end points
+#1. Allow user to see all the information about them selves, that is return record from
+# database to the user.
+#2. Allow users to change their password.
