f62dc88c0031790cc3f70d7ca2c228a0408b62d2
a3malik <amit26klm@gmail.com>
2025-06-27 21:08:59 +0530

    added testing

--- todoapp/database.py
+++ todoapp/database.py
@@ -2,7 +2,7 @@
 # This file sets up the database connection and session management for the FastAPI application.
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 #SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:urpasswd@localhost/TodoApplicationDatabase'
