cd10f929b2786ad71f9797ba3f6a70342b45abf6
a3malik <amit26klm@gmail.com>
2025-07-01 20:13:25 +0530

    more tests for user endpoint

--- todoapp/tests/test_users.py
+++ todoapp/tests/test_users.py
@@ -14,3 +14,22 @@ def test_return_user(test_user):
     assert response.json()['last_name'] == 'Test'
     assert response.json()['role'] == 'admin'
     assert response.json()['phone_number'] == '1234567890'
+
+def test_change_password_success(test_user):
+    response = client.put("/user/password", json={
+                                            "password": "password",
+                                            "new_password": "newpassword"
+                                        }
+                            )
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+def test_change_password_fail(test_user):
+    response = client.put("/user/password", json={'password':'password!',
+                                                  'new_password':'newpassword'}
+                                                  )
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == {'detail':'Error on user verification'}
+
+def test_change_phone_number_success(test_user):
+    response = client.put("/user/phonenumber/1111111111")
+    assert response.status_code == status.HTTP_204_NO_CONTENT
