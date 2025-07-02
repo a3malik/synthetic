47a636a6c4311d59c639266d59953b483748548c
a3malik <amit26klm@gmail.com>
2025-07-02 19:01:15 +0530

    added tests for auth endpoint

---
+++ todoapp/tests/test_auth.py
@@ -0,0 +1,33 @@
+from .utils import *
+from fastapi import status
+from ..routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM
+from jose import jwt
+from datetime import timedelta
+
+app.dependency_overrides[get_db] = override_get_db
+
+def test_authenticate_user(test_user):
+    db = TestingSessionLocal()
+    authenticated_user = authenticate_user("akmaliktest", "password", db)
+    assert authenticated_user is not None
+    assert authenticated_user.username == test_user.username
+
+    non_existent_user = authenticate_user("WrongUserName", "password", db)
+    assert non_existent_user is False
+
+    wrong_passwrord_user = authenticate_user(test_user.username, "wrongpassword", db)
+    assert wrong_passwrord_user is False
+
+def test_create_access_token():
+    username = 'testuser'
+    user_id = 1
+    role = 'users'
+    expires_delta = timedelta(days=1)
+
+    token = create_access_token(username, user_id, role, expires_delta)
+    decoded_token = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM], 
+                               options={"verify_signature": False})
+    
+    assert decoded_token['sub'] == username
+    assert decoded_token['id'] == user_id
+    assert decoded_token['role'] == role
