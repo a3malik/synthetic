308dfc74d2586c0351ebac8c7b257499fd7e0d3e
a3malik <amit26klm@gmail.com>
2025-07-05 20:05:46 +0530

    more tests for auth endpoint

--- todoapp/tests/test_auth.py
+++ todoapp/tests/test_auth.py
@@ -1,8 +1,11 @@
 from .utils import *
 from fastapi import status
-from ..routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM
+from ..routers.auth import get_db, authenticate_user, create_access_token, \
+SECRET_KEY, ALGORITHM, get_current_user
 from jose import jwt
 from datetime import timedelta
+import pytest
+from fastapi import HTTPException
 
 app.dependency_overrides[get_db] = override_get_db
 
@@ -31,3 +34,22 @@ def test_create_access_token():
     assert decoded_token['sub'] == username
     assert decoded_token['id'] == user_id
     assert decoded_token['role'] == role
+
+@pytest.mark.asyncio
+async def test_get_current_user_valid_token():
+    encode = {'sub':'testuser','id':1,'role':'admin'}
+    token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    user = await get_current_user(token=token)
+    assert user == {'username':'testuser','id':1, 'user_role':'admin'}
+
+@pytest.mark.asyncio
+async def test_get_current_user_missing_payload():
+    encode = {'role':'user'}
+    token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    with pytest.raises(HTTPException) as excinfo:
+        await get_current_user(token=token)
+
+    assert excinfo.value.status_code == status.HTTP_401_UNAUTHORIZED
+    assert excinfo.value.detail == 'Could not validate user.'
